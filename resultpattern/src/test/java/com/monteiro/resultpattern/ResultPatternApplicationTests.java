e90f4744688fec5406de6561f410d7a879bd0d5c
Monteiro <monteirolnx@outlook.com>
2024-10-01 21:03:21 -0300

    Add .gitignore files for IntelliJ IDEA and VS Code

---
+++ resultpattern/src/test/java/com/monteiro/resultpattern/ResultPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monteiro.resultpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
